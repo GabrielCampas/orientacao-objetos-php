210a820b9f1edc36ae178528bba48f4023f2682b
GABRIEL CAMPANHA <0201392521005>
2026-03-11 13:08:59 -0300

    Orientação a Objetos em PHP

--- Classe/fornecedor.class.php
+++ Classe/fornecedor.class.php
@@ -1,12 +1,32 @@
 <?php
     class Fornecedor{
-        private string $razaoSocial = "FATEC";
-        private string $cnpj = "111.111.111-11";
-        private string $celular = "(14) 99710-1010";
-        private string $email = "fornecedor@gmail.com";
+        public function __construct(private string $razaoSocial="FATEC", private string $cnpj = "111.111.111-11", private string $celular="(14) 99710-1010", private string $email="fornecedor@gmail.com"){
+
+        }
+        
+        //private string $razaoSocial = "FATEC";
+        //private string $cnpj = "111.111.111-11";
+        //private string $celular = "(14) 99710-1010";
+        //private string $email = "fornecedor@gmail.com";
         
         public function Exibindo(){
             echo "<strong>Razão Social:</strong> {$this->razaoSocial} <br> <strong>CNPJ:</strong> {$this->cnpj} <br> <strong>Celular:</strong> {$this->celular} <br> <strong>Email: </strong>{$this->email}.";
+        }
+
+        public function getRazao(){
+            return $this->razaoSocial;
+        }
+
+        public function getCNPJ(){
+            return $this->cnpj;
+        }
+
+        public function getCelular(){
+            return $this->celular;
+        }
+
+        public function getEmail(){
+            return $this->email;
         }
     }
 ?>
