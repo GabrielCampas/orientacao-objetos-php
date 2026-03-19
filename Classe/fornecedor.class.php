f8a9f35ec68807a26aa7bdc698460dd9c96b9b99
Gabriel Campanhã <campanhag101@gmail.com>
2026-03-19 16:09:46 -0300

    orientação a objetos em pgp

--- Classe/fornecedor.class.php
+++ Classe/fornecedor.class.php
@@ -1,13 +1,8 @@
 <?php
     class Fornecedor{
-        public function __construct(private string $razaoSocial="FATEC", private string $cnpj = "111.111.111-11", private string $celular="(14) 99710-1010", private string $email="fornecedor@gmail.com"){
+        public function __construct(private string $razaoSocial="", private string $cnpj = "", private string $celular="", private string $email=""){
 
         }
-        
-        //private string $razaoSocial = "FATEC";
-        //private string $cnpj = "111.111.111-11";
-        //private string $celular = "(14) 99710-1010";
-        //private string $email = "fornecedor@gmail.com";
         
         public function Exibindo(){
             echo "<strong>Razão Social:</strong> {$this->razaoSocial} <br> <strong>CNPJ:</strong> {$this->cnpj} <br> <strong>Celular:</strong> {$this->celular} <br> <strong>Email: </strong>{$this->email}.";
--- Classe/indexfornecedor.php
+++ Classe/indexfornecedor.php
@@ -1,7 +1,7 @@
 <?php
     require_once "fornecedor.class.php";
 
-    $fornecedor = new Fornecedor();
+    $fornecedor = new Fornecedor("Silcar Pneus", "10.100.100/0001-10", "(14)997901010", "silcar@gmail.com");
 
     echo "Exibindo informações do fornecedor: ";
     echo "<br><br>";
